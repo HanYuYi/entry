eabf984af7f538b6a2306389cc45b3696c3a64af
baron <marvinhan884@gmail.com>
2022-03-25 08:13:16 +0800

    2022-3-24 servlet Filter

--- src/main/java/com/HanYuYi/web/filter/FilterServlet.java
+++ src/main/java/com/HanYuYi/web/filter/FilterServlet.java
@@ -4,25 +4,45 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 /**
- * Filter: 在/user/*路径下到达HttpServlet之前会执行，一般用于处理公共逻辑
+ * Filter: 在/filter_page.html路径下到达HttpServlet之前会执行，一般用于处理公共逻辑
  * 如果有多个filter，可以在web.xml种指定顺序
+ * 三个生命周期
  */
-@WebFilter("/user/*")
-public class FilterServlet implements Filter {
 
+// 推荐使用配置，不推荐使用注解，因为filter如果有多个，需要指定顺序
+// @WebFilter("/filter_page.html")
+public class FilterServlet implements Filter {
+    private String encoding = "";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String encoding = filterConfig.getInitParameter("encoding");
+        this.encoding = encoding;
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletRequest.setCharacterEncoding("utf-8");
-        servletResponse.setCharacterEncoding("utf-8");
-        // 要继续处理请求，必须调用doFilter，但如果逻辑未完成可以重定向
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        if (false) {
-            response.sendRedirect("/");
-        } else {
+        if (encoding != "" && encoding != null) {
+            String reqEncoding = servletRequest.getCharacterEncoding();
+            String respEncoding = servletResponse.getCharacterEncoding();
+            if (reqEncoding == null) {
+                servletRequest.setCharacterEncoding("utf-8");
+            }
+            if (respEncoding == null) {
+                servletResponse.setCharacterEncoding("utf-8");
+            }
+            // 要继续处理请求，必须调用doFilter，但如果逻辑未完成可以重定向
             filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            String virPath = servletRequest.getServletContext().getContextPath();
+            response.sendRedirect(virPath + "/register.html");
         }
     }
+
+    @Override
+    public void destroy() {}
 }
