70f25fa7cbaf7780398223c3bc2a4fae98ccf05f
baron <marvinhan884@gmail.com>
2022-04-01 22:30:16 +0800

    2022-4-1 spring

---
+++ spring/src/main/java/com/HanYuYi/javaConfig/config/BeanConfig.java
@@ -0,0 +1,22 @@
+package com.HanYuYi.javaConfig.config;
+
+import com.HanYuYi.javaConfig.user.User;
+import org.springframework.context.annotation.*;
+
+/**
+ * 演示使用纯java代码将用spring管理bean
+ * @Configuration 声明是一个javaConfig
+ * @ComponentScan 扫描包
+ * @Import 跟xml里的import一样
+ */
+@Import(BeanConfig1.class)
+@Configuration
+@ComponentScan("com.HanYuYi.javaConfig.user")
+@Scope("prototype")
+public class BeanConfig {
+
+    @Bean
+    public User getUser() {
+        return new User();
+    }
+}
---
+++ spring/src/main/java/com/HanYuYi/javaConfig/config/BeanConfig1.java
@@ -0,0 +1,7 @@
+package com.HanYuYi.javaConfig.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig1 {
+}
