fea1272d03a77e1911fd17d875e43c8aeb39ac16
baron <marvinhan884@gmail.com>
2022-05-19 16:47:01 +0800

    2022-5-19 优化mybatis-spring这整合

--- admin/src/main/java/com/HanYuYi/dao/mapper/SupplierTableMapperImpl.java
+++ admin/src/main/java/com/HanYuYi/dao/mapper/SupplierTableMapperImpl.java
@@ -1,14 +1,12 @@
 package com.HanYuYi.dao.mapper;
 
 import com.HanYuYi.entity.SupplierTable;
-import org.apache.ibatis.annotations.Param;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 /**
  * 供应商
  */
 public class SupplierTableMapperImpl extends SqlSessionDaoSupport implements SupplierTableMapper {
-
     /**
      * 新增供应商
      * @param Supplier
