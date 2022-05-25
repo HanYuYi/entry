a34b5dc7d1d27633843c1d94c75ece31cc44dfaa
baron <marvinhan884@gmail.com>
2022-05-25 17:40:36 +0800

    2022-5-25fix

--- spring-mvc-study/src/main/java/com/HanYuYi/controller/HomeController.java
+++ spring-mvc-study/src/main/java/com/HanYuYi/controller/HomeController.java
@@ -4,17 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
+@RequestMapping("/page")
 public class HomeController {
 
     @RequestMapping("/home.do")
     public String homePage() {
-        System.out.println("home");
+        System.out.println("/home.do");
         return "home";
     }
 
     @RequestMapping("home2.do")
     public ModelAndView homePageByModelANdView() {
+        System.out.println("/home2.do");
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("msg", "你好，spring-MVC！");
         modelAndView.setViewName("home");
