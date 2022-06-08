5d9a9c5ef9356fc1240acd2c7cecc806668a63b4
baron <marvinhan884@gmail.com>
2022-06-08 22:45:04 +0800

    2022-6-8 ssm整合

--- blog-ssm/src/main/java/com/HanYuYi/controller/BlogController.java
+++ blog-ssm/src/main/java/com/HanYuYi/controller/BlogController.java
@@ -2,7 +2,6 @@ package com.HanYuYi.controller;
 
 import com.HanYuYi.pojo.Blog;
 import com.HanYuYi.service.BlogService;
-import com.HanYuYi.service.BlogServiceImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -18,11 +18,12 @@ import java.util.List;
 @RequestMapping("/blog")
 public class BlogController {
 
-//    @Autowired
-//    @Qualifier("BlogServiceImpl")
-    private BlogService blogService = new BlogServiceImpl();
+    @Autowired
+    @Qualifier("BlogServiceImpl")
+    private BlogService blogService;
 
     @RequestMapping("/getBlog")
+    @ResponseBody
     public String getBlogs() throws JsonProcessingException {
         List<Blog> blogs = blogService.getBlogsAll();
 
