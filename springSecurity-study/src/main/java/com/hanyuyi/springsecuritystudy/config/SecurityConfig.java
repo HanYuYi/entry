04b7895f409cd1d5c4da77797e1775c790c1f1c8
baron <marvinhan884@gmail.com>
2022-08-26 23:09:13 +0800

    2022-8-26

--- springSecurity-study/src/main/java/com/hanyuyi/springsecuritystudy/config/SecurityConfig.java
+++ springSecurity-study/src/main/java/com/hanyuyi/springsecuritystudy/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 设置vip角色的用户只能访问 /level1/ 下的资源
                 .antMatchers("/level1/**").hasRole("vip1")
                 .antMatchers("/level2/**").hasRole("vip2")
-                .antMatchers("/level3/**").hasRole("vip3");
+                .antMatchers("/level3/**").hasAuthority("p1");
 
         // 显示开启 没有权限跳转到登录页面，这个是spring security的登录页
         http.formLogin();
@@ -51,7 +51,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("lisi").password(new BCryptPasswordEncoder().encode("123456")).roles("vip2", "vip3")
                 .and()
-                .withUser("wangwu").password(new BCryptPasswordEncoder().encode("123456")).roles("vip3");
-
+                .withUser("wangwu").password(new BCryptPasswordEncoder().encode("123456")).authorities("p1");
     }
 }
