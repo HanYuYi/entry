6616c9d3eb405aae1021af671f5df7f541ce9989
baron <marvinhan884@gmail.com>
2022-12-10 17:20:43 +0800

    成功将缓存放进redis

--- springboot-project/src/main/java/com/example/springbootproject/config/RedisConfig.java
+++ springboot-project/src/main/java/com/example/springbootproject/config/RedisConfig.java
@@ -2,7 +2,6 @@ package com.example.springbootproject.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.JedisPool;
--- springboot-project/src/main/java/com/example/springbootproject/service/BlogServiceImpl.java
+++ springboot-project/src/main/java/com/example/springbootproject/service/BlogServiceImpl.java
@@ -2,17 +2,14 @@ package com.example.springbootproject.service;
 
 import com.example.springbootproject.mapper.BlogMapper;
 import com.example.springbootproject.pojo.Blog;
-import com.example.springbootproject.utils.DataConversion;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -39,31 +36,8 @@ public class BlogServiceImpl implements BlogService {
     @Cacheable(value = "blog", key = "'userBlog' + #username")
     @Override
     public List<Blog> getBlogs(String username) {
-
-        String rBlogKey = username + ":blog";
-
-        Jedis resource = jedisPool.getResource();
-
-        List<String> rBlogList = resource.lrange(rBlogKey, 0, -1);
-
-        List<Blog> backList = new ArrayList<>();
-
-        if (rBlogList.size() > 0) {
-            for (String bolg : rBlogList) {
-                backList.add((Blog) DataConversion.Deserialization(bolg, new Blog()));
-            }
-            log.info("从redis查询的日志：");
-        } else {
-            backList = blogMapper.getBlogsAll();
-            for (Blog blog : backList) {
-                resource.lpush(rBlogKey, DataConversion. serialize(blog));
-            }
-            resource.expire(rBlogKey, 1 * 60 * 60);
-            log.info("从mysql查询的日志：");
-        }
-        log.info(backList.toString());
-
-        resource.close();
+        List<Blog> backList = blogMapper.getBlogsAll();
+        System.out.println(backList);
 
         return backList;
     }
